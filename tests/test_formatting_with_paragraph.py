268046f5eb67c613472791676998c2fed09fcbd5
Iulian Ciorascu <iulica@iclic.ro>
2024-12-28 11:34:26 +0100

    Added test (expected failure) for the case where there is a nested field in a normal text field. In the "before" text.

--- tests/test_formatting_with_paragraph.py
+++ tests/test_formatting_with_paragraph.py
@@ -6,7 +6,7 @@ from tests.utils import EtreeMixin
 
 
 class BeforeAfterWithParTest(EtreeMixin, unittest.TestCase):
-    def test_number(self):
+    def test_paragraph(self):
         values = ["one", "two", "three"]
         document, root_elem = self.merge_templates(
             "test_field_with_paragraph.docx",
@@ -22,4 +22,23 @@ class BeforeAfterWithParTest(EtreeMixin, unittest.TestCase):
 
         fields = root_elem.xpath("//w:t/text()", namespaces=NAMESPACES)
         expected = [v for value in values for v in ["before", f"par{value} after ", "par"]]
+        self.assertListEqual(fields, expected)
+
+    @unittest.expectedFailure
+    def test_paragraph_and_nested_field(self):
+        values = ["one", "two", "three"]
+        document, root_elem = self.merge_templates(
+            "test_field_with_paragraph_and_nested_field.docx",
+            [
+                {
+                    "fieldname": value,
+                }
+                for value in values
+            ],
+            separator="nextPage_section",
+            output="tests/output/test_output_field_with_paragraph_and_nested_field.docx",
+        )
+
+        fields = root_elem.xpath("//w:t/text()", namespaces=NAMESPACES)
+        expected = [v for value in values for v in ["before", f"par {value} {value} after ", "par"]]
         self.assertListEqual(fields, expected)
